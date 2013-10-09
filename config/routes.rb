868d4a71c572af12eac4939a1751cd1ec0094732
Des Power <despower1@mac.com>
2013-10-09 17:42:20 +0100

    Added controller for Pages - root to home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 App11::Application.routes.draw do
+root :to => 'Pages#home'
+
+
+  get 'about' => "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
